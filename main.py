932766114c43da7c0caef0d87d2c83c17d63700b
Oliver <Oliver@AVMACBOOKPROOPOE.local>
2017-09-18 15:13:21 -0700

    minor cleanup

--- main.py
+++ main.py
@@ -10,8 +10,12 @@ count_cmd = bytes([0b00010110])
 gpio_cmd = bytes([0b00000100])
 alarm_en_cmd = bytes([0b00001000])
 
+# An object that handles macroscopic 6651 settings.
 class Max6651(object):
-    # An object that handles macroscopic 6651 settings.
+    def read_reg(self, cmd):
+        i2c.writeto(self.addr, cmd)
+        return i2c.readfrom(self.addr, 1) 
+
     def __init__(self, addr_raw, full_on_raw, tach_overflow_raw):
         self.addr = addr_raw
         self.tach_count_time = 2 
@@ -33,18 +37,14 @@ class Max6651(object):
         # calculate max rpm and prescale then set max6651 settings
         time.sleep_ms(5) # probably not needed. 
         self.max_rps = self.find_max_speed()
-        self.tach_prescaler_bits = self.calc_prescale(self.max_rps)
+        self.tach_prescaler_bits = self.calc_prescale()
         config_val = 0b00101 #closed-loop operation, 12v fan operation
         config_val = ( config_val << 3) + self.tach_prescaler_bits
         config = config_cmd + bytes([config_val])    
         i2c.writeto(self.addr, config)
         # read for debug
-        #i2c.writeto(self.addr, config_cmd)
-        #config_reg = i2c.readfrom(self.addr, 1) 
+        print(read_reg(config_cmd))
 
-    def read_reg(self, cmd):
-        i2c.writeto(self.addr, cmd)
-        return i2c.readfrom(self.addr, 1) 
 
     def read_speed(self,tach_cmd):
         # fan speed value is between 0 -> 255, reusing equation on pg11 of datasheet
@@ -72,26 +72,25 @@ class Max6651(object):
         self.full_on_pin.value(1)
         return fan0_rps
 
-    def calc_prescale(self, max_rps):
-        # calculate ideal prescaler value for closed loop operation (target = 0d64)
+    def calc_prescale(self):
+        # calculate ideal prescaler value for closed loop operation (target = 64)
         # equation 1 on pg 21 
         prescale_dict = {1 : 0b000 , 2 : 0b001 , 4 : 0b010 , 8 : 0b011 , 16 : 0b100}
-        tach_prescaler_tmp = max_rps * 64 / 992 
+        tach_prescaler_tmp = self.max_rps * 64 / 992 
         tach_prescaler = min(prescale_dict, key=lambda x:abs(x-tach_prescaler_tmp)) # find closest value
         # debug 
-        ccccccccccccccc
         #print('tach_prescaler:', tach_prescaler)
-        #print("tach_prescale:",prescale_dict[tach_prescaler])
+        #print("tach_prescaler_bits:",prescale_dict[tach_prescaler])
         return prescale_dict[tach_prescaler] # return 3 bit value
 
-    def set_target_speed(self, target_percentage, max_rps):
+    def set_target_speed(self, target_percentage):
             tach_prescaler = 2**self.tach_prescaler_bits
-            target_rps = (target_percentage / 100) * max_rps
+            target_rps = (target_percentage / 100) * self.max_rps
             speed = round( (992 * tach_prescaler / target_rps) - 1 ) 
             print('tach_prescaler:', tach_prescaler)
             print('target:',  target_rps)
             print('calculated speed val:', speed)
-            speed_word = bytes([0,int(speed)])
+            speed_word = speed_cmd + bytes([speed])
             i2c.writeto(self.addr, speed_word)
             assert self.read_reg(speed_cmd) == speed_word
                 
@@ -100,7 +99,7 @@ scl = pyb.Pin.board.Y9
 sda = pyb.Pin.board.Y10
 i2c = I2C(-1,scl,sda,freq=200000)
 # pin x1 connected to max6651 gpio1 for full_on operation,
-# pin x2 comnected to max6651 gpio 0 for tach count overflow
+# pin x2 connected to max6651 gpio 0 for tach count overflow
 zone1 = Max6651(72, 'X1', 'X2')
 print ( zone1.read_speed(tach0_cmd) )
 #zone1.set_target_speed(50, 40)
